72a11a007afc8306e78e5f965de3fb6ec63000dc
IvelinaIlieva <90511028+IvelinaIlieva@users.noreply.github.com>
2022-06-11 19:03:30 +0300

    Add Lab

---
+++ Generics/Lab/Box.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    internal class Box<T>
+    {
+        public int Count => this.BoxStack.Count;
+        public Stack<T> BoxStack { get; set; }
+
+        public Box()
+        {
+            this.BoxStack = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.BoxStack.Push(element);
+        }
+
+        public T Remove()
+        {
+           return this.BoxStack.Pop();
+        }
+
+    }
+}
