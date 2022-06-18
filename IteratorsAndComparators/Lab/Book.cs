56486bafc7274cd1d54d2c4e9280f6be20e21331
IvelinaIlieva <90511028+IvelinaIlieva@users.noreply.github.com>
2022-06-18 13:10:42 +0300

    Initial Commit
    Add Lab

---
+++ IteratorsAndComparators/Lab/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        private string title;
+        private int year;
+        private IReadOnlyList<string> author;
+
+        public string Title { get => title; set => title = value; }
+
+        public int Year { get => year; set => year = value; }
+
+        public IReadOnlyList<string> Author { get => author; set => author = value; }
+
+        public Book(string title, int year, params string[] author)
+        {
+            Title = title;
+            Year = year;
+            Author = author;
+        }
+
+        public int CompareTo(Book other)
+        {
+            int result = this.Year.CompareTo(other.Year);
+
+            if (result == 0)
+            {
+                return this.Title.CompareTo(other.Title);
+            }
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.title} - {this.year}";
+        }
+    }
+}
---
+++ IteratorsAndComparators/Lab/Library.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator() => new LibraryIterator(this.books);
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books.OrderBy(b => b.CompareTo(b)));
+            }
+
+            public bool MoveNext() => currentIndex++ < this.books.Count;
+            public void Reset() => currentIndex = 0;
+            public Book Current => this.books[currentIndex - 1];
+            object IEnumerator.Current => Current;
+            public void Dispose() { }
+        }
+    }
+}
